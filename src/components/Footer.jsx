1b132e75c3450fd4c10a8866c65da42b118c0d87
Mohammad Amin Feli Akbari <mfly22317@gmail.com>
2024-12-02 10:41:35 +0330

    add sign up menu without login backend

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
     <footer className="bg-white shadow dark:bg-gray-800">
       <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
         <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-          © 2023{" "}
+          © 2023
           <NavLink to="https://flowbite.com/" className="hover:underline">
             Flowbite™
           </NavLink>
@@ -24,7 +24,7 @@ export default function Footer() {
               rel="noopener noreferrer"
             >
               Git Hub
-            </a> 
+            </a>
           </li>
           <li>
             <NavLink
---
+++ src/components/TextSection.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./signUpCss.css";
+function TextSection() {
+  return (
+    <div className="text-container text-purple-950 text-center drop-shadow-2xl">
+      <h2 className="heading font-bold font-">BUY NOW</h2>
+      <p className="paragraph font-semibold">
+        See how experienced developers solve problems in real-time. Watching
+        scripted tutorials is great, but understanding how developers think is
+        invaluable.
+      </p>
+    </div>
+  );
+}
+
+export default TextSection;
