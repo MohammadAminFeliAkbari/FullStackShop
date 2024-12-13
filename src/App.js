a156f4f32731c9c97ea960524cd25a6ae673c6ae
Mohammad Amin Feli Akbari <mfly22317@gmail.com>
2024-12-13 22:41:31 +0330

    reset app.js file to original

--- src/App.js
+++ src/App.js
@@ -1,32 +1,21 @@
+import AllRoute from './allRoute'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { createContext, useState } from 'react'
 
-import { Navigation, Pagination } from 'swiper/modules';
-// Direct React component imports
-import { Swiper, SwiperSlide } from 'swiper/swiper-react.mjs';
+// Step 1: Create and export AppContext
+export const AppContext = createContext(null)
 
-// Styles must use direct files imports
-import 'swiper/swiper.scss'; // core Swiper
-import 'swiper/modules/navigation/navigation.scss'; // Navigation module
-import 'swiper/modules/pagination/pagination.scss'; // Pagination module
-// Import Swiper styles 
+export default function App () {
+  const [usernameLogin, setUsernameLogin] = useState('')
 
-export default () => {
   return (
-    <Swiper
-      // install Swiper modules
-      modules={[Navigation, Pagination, Scrollbar, A11y]}
-      spaceBetween={50}
-      slidesPerView={3}
-      navigation
-      pagination={{ clickable: true }}
-      scrollbar={{ draggable: true }}
-      onSwiper={(swiper) => console.log(swiper)}
-      onSlideChange={() => console.log('slide change')}
-    >
-      <SwiperSlide>Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
-      ...
-    </Swiper>
-  );
-};
+    <AppContext.Provider value={{ setUsernameLogin, usernameLogin }}>
+      <Header />
+      <main>
+        <AllRoute />
+      </main>
+      <Footer />
+    </AppContext.Provider>
+  )
+}
